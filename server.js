16103867751e7faed5ec0946df5d6fbb78ccc448
Yuta4u <yudhistira4u@gmail.com>
2024-06-11 05:58:10 +0700

    solve error deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 require("dotenv").config()
+const bodyParser = require("body-parser")
 const serverless = require("serverless-http")
 const cors = require("cors")
 
